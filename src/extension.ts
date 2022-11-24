419a54ec36e61694813b37abd221bff912c4f5e9
Yair Haimo <yair.haimo@gmail.com>
2022-11-24 21:44:20 +0200

    generic commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as codeint from '@code-int/analytics';
+
 const memoize = require('lodash.memoize');
 
 const getFileToOpen = memoize(
 	async (dirName: string, patterns: RegExp[]) => {
-		const files = (await vscode.workspace.fs.readDirectory(vscode.Uri.file(dirName))).map((file) => file[0]);
+		const files = (await vscode.workspace.fs.readDirectory(vscode.Uri.file(dirName))).map(
+			(file) => file[0]
+		);
 		return findFileByPatternPriority(files, patterns);
 	},
 	(...args: any[]) => args.join('@')
@@ -15,26 +17,6 @@ function getCurrentDirName() {
 	const filePath = vscode.window.activeTextEditor?.document.fileName;
 	if (filePath) {
 		return path.dirname(filePath);
-	}
-}
-
-async function openTestFile() {
-	openFile([/(?:test|spec)/]);
-}
-
-async function openDriverFile() {
-	openFile([/driver/]);
-}
-
-async function openStyleFile() {
-	openFile([/\.(?:sass|scss|css|less)/]);
-}
-
-async function openImplFile() {
-	const dirName = getCurrentDirName();
-	if (dirName) {
-		const dirNameRegex = new RegExp(`${dirName.split('/').slice(-1)[0]}(?:\.comp)?\.[jt]sx?`, 'i');
-		openFile([dirNameRegex, /index\.[jt]sx?/]);
 	}
 }
 
@@ -63,23 +45,16 @@ async function openFile(patterns: RegExp[]) {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	codeint.init('6933510f-7c3e-4f19-941b-8756ad505d69', context);
-	const testDisposable = codeint.registerCommand('extension.switcheroonyToTest', async () => {
-		await openTestFile();
+	const conf = vscode.workspace.getConfiguration('switcheroony');
+	console.log(`***conf`, conf);
+
+	['One', 'Two', 'Three', 'Four'].forEach((num) => {
+		context.subscriptions.push(
+			vscode.commands.registerCommand(`extension.switcheroony${num}`, async () => {
+				await openFile([new RegExp(conf[num.toLowerCase()].pattern)]);
+			})
+		);
 	});
-	context.subscriptions.push(testDisposable);
-	const prodDisposable = codeint.registerCommand('extension.switcheroonyToImplementation', async () => {
-		await openImplFile();
-	});
-	context.subscriptions.push(prodDisposable);
-	const driverDisposable = codeint.registerCommand('extension.switcheroonyToDriver', async () => {
-		await openDriverFile();
-	});
-	context.subscriptions.push(driverDisposable);
-	const styleDisposable = codeint.registerCommand('extension.switcheroonyToStyle', async () => {
-		await openStyleFile();
-	});
-	context.subscriptions.push(styleDisposable);
 }
 
 // this method is called when your extension is deactivated
